d9eba9a4db7612550b10c931e9fbbda0f1fb4694
ShohKhan-dev <shoh.hamraqulov@gmail.com>
2024-04-18 13:00:04 +0500

    fix trps

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -15,7 +15,7 @@ const Providers = ({ children }: PropsWithChildren) => {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: 'http://localhost:3000/api/trpc',
+          url: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/trpc`,
         }),
       ],
     })
